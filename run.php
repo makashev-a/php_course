ed688161f9dbd4b5d16e7306b1ae7ff10fcb2f33
Akhmadi <ahmadi98ahmadi@gmail.com>
2021-05-30 23:07:45 +0600

    function

--- run.php
+++ run.php
@@ -1,50 +1,12 @@
 <pre>
 <?php
 
-$todos = [
-    [
-        "id" => 1,
-        "title" => "Купить столовые приборы",
-        "date" => "03.04.2020",
-        "status" => "Выполнено"
-    ],
-    [
-        "id" => 2,
-        "title" => "Выбросить мусор",
-        "date" => "05.04.2020",
-        "status" => "Ожидается"
-    ],
-    [
-        "id" => 3,
-        "title" => "Помыть машину",
-        "date" => "10.04.2020",
-        "status" => "Ожидается"
-    ]
-];
-
-for ($i = 0; $i < count($todos); $i++) {
-    echo "id - " . $todos[$i]["id"] . "\n";
-    echo "Заголовок - " . $todos[$i]["title"] . "\n";
-    echo "Дата - " . $todos[$i]["date"] . "\n";
-    echo "Статус - " . $todos[$i]["status"] . "\n\n";
+function discriminant($a, $b, $c)
+{
+    return pow($b, 2) - (4 * $a * $c);
 }
 
-$i = 0;
-
-while ($i < count($todos)) {
-    echo "id - " . $todos[$i]["id"] . "\n";
-    echo "Заголовок - " . $todos[$i]["title"] . "\n";
-    echo "Дата - " . $todos[$i]["date"] . "\n";
-    echo "Статус - " . $todos[$i]["status"] . "\n\n";
-    $i++;
-}
-
-foreach ($todos as $todo) {
-    echo "id - " . $todo["id"] . "\n";
-    echo "Заголовок - " . $todo["title"] . "\n";
-    echo "Дата - " . $todo["date"] . "\n";
-    echo "Статус - " . $todo["status"] . "\n\n";
-}
+echo discriminant(3, -14, -5);
 
 ?>
 </pre>
