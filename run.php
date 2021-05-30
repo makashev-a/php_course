35890ffd43d22f4cc8b4b54f2b5d4757184c5610
Akhmadi <ahmadi98ahmadi@gmail.com>
2021-05-30 22:40:40 +0600

    for, while, foreach

--- run.php
+++ run.php
@@ -1,39 +1,53 @@
+<pre>
 <?php
 
-const LOGIN = "admin",
-PASSWORD = "secret123";
+$todos = [
+    [
+        "id" => 1,
+        "title" => "Купить столовые приборы",
+        "date" => "03.04.2020",
+        "status" => "Выполнено"
+    ],
+    [
+        "id" => 2,
+        "title" => "Выбросить мусор",
+        "date" => "05.04.2020",
+        "status" => "Ожидается"
+    ],
+    [
+        "id" => 3,
+        "title" => "Помыть машину",
+        "date" => "10.04.2020",
+        "status" => "Ожидается"
+    ]
+];
 
-$tmp_login = "admin";
-$tmp_password = "secret123";
-
-if ($tmp_login == LOGIN && $tmp_password == PASSWORD) {
-    echo "Authorization is successfull!<br>";
-} elseif ($tmp_login == LOGIN && $tmp_password != PASSWORD) {
-    echo "Please check your passsword!<br>";
-} elseif ($tmp_login != LOGIN) {
-    echo "User not found!<br>";
+for ($i = 0; $i < count($todos); $i++) {
+    echo "id - " . $todos[$i]["id"] . "\n";
+    echo "Заголовок - " . $todos[$i]["title"] . "\n";
+    echo "Дата - " . $todos[$i]["date"] . "\n";
+    echo "Статус - " . $todos[$i]["status"] . "\n\n";
 }
 
-$isStatus = false;
-$statusResult = ($isStatus == true) ? 43224656 : 98645623;
-echo "Status = " . $statusResult . "<br>";
+$i = 0;
 
-$typeCar = 'BMW';
-
-switch ($typeCar) {
-    case "BMW":
-        echo 'BMW is a very good car';
-        break;
-    case "Toyota":
-        echo 'Toyota is a very reliable car!';
-        break;
-    case "Lada":
-        echo "Don't buy these cars!";
-        break;
-    default:
-        echo 'Car not found';
-        break;
+while ($i < count($todos)) {
+    echo "id - " . $todos[$i]["id"] . "\n";
+    echo "Заголовок - " . $todos[$i]["title"] . "\n";
+    echo "Дата - " . $todos[$i]["date"] . "\n";
+    echo "Статус - " . $todos[$i]["status"] . "\n\n";
+    $i++;
 }
+
+foreach ($todos as $todo) {
+    echo "id - " . $todo["id"] . "\n";
+    echo "Заголовок - " . $todo["title"] . "\n";
+    echo "Дата - " . $todo["date"] . "\n";
+    echo "Статус - " . $todo["status"] . "\n\n";
+}
+
+?>
+</pre>
 
 
 
