fe651e5ff6a21212a98ccd637d54008b03638a33
Akhmadi <ahmadi98ahmadi@gmail.com>
2021-06-01 21:43:26 +0600

    fix

--- run.php
+++ run.php
@@ -42,3 +42,20 @@
         padding: 8px;
     }
 </style>
+
+<form action="vendor/add_product.php" method="post" id="products">
+    <p>Название товара</p>
+    <input type="text" name="name">
+    <p>Описание товара</p>
+    <textarea name="description"></textarea> <br><br>
+    <p>Категория товара</p>
+    <select name="category_id" form="products">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+    </select>
+    <p>Цена товара</p>
+    <input type="number" name="price"> <br><br>
+    <button type="submit">Опубликовать пост</button>
+</form>
